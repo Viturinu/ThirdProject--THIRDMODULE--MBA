5b8ec24002f94103c385bd084d2e2870c502acc0
Victor Oliveira Almeida <victor.almeida.ti.celular@gmail.com>
2025-05-06 12:39:08 -0300

    colocar todos os tests

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -2,20 +2,29 @@ import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-reposi
 import { User } from "generated/prisma"
 import { expect, describe, it } from "vitest"
 import { RegisterUSeCase } from "./register"
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository"
+import { UserAlreadyExistsError } from "./errors/user-already-exists"
 
 describe("Register Use Case", () => {
     it("should have user password upon registration", async () => {
-        const prismaUsersRepository = new PrismaUsersRepository()
+        const prismaUsersRepository = new InMemoryUsersRepository() //intead of new PrismaUsersRepository(), we set new InMemoryUsersRepository() - simulation of Repository
         const registerUseCase = new RegisterUSeCase(prismaUsersRepository)
 
-        const user = await registerUseCase.execute({
+        const email = "johndoe@gmail.com"
+
+        const { user } = await registerUseCase.execute({
             name: "John Doe",
-            email: "johndoe@gmail.com",
+            email,
             password: "123456",
         })
 
-        return {
-            user
-        }
+        expect(user.id).toEqual(expect.any(String))
+
+        await expect(() => registerUseCase.execute({ //await porque espera uma Promise com rejects ali embaixo
+            name: "John doe",
+            email,
+            password: "123456",
+        }),
+        ).rejects.toBeInstanceOf(UserAlreadyExistsError)
     })
 })
